cd9df2ab056ca46b3a2ab4eb3702365f5bb99e8a
TeunMos <teun.mos@gmail.com>
2022-11-07 15:10:09 +0100

    added test for GetCard method in urlCard tests
    Co-authored-by: Lukas <LukasJansen100@users.noreply.github.com>

--- tests/test_urlCards.py
+++ tests/test_urlCards.py
@@ -1,13 +1,28 @@
 import pytest
+import uuid
+import json
 from routers import urlCards
 import mongomock
+from models.UrlModels import UrlCard
+
+collection_mock = mongomock.MongoClient().mockurl_db.mockurl_collection
 
 def createMockDB():
-    collection_mock = mongomock.MongoClient().mockurl_db.mockurl_collection
     urlCards.urldb = collection_mock
 
 
 @pytest.mark.asyncio
-async def test():
+async def test_GetCard_returns_CorrectCard():
+
+    #arrange
     createMockDB()
-    assert 0 == await urlCards.AddUrlCard()
+    urlcard = UrlCard(cardId=str(uuid.uuid4()), Urls=[])
+    collection_mock.insert_one(dict(urlcard))
+
+    #act
+    result = await urlCards.GetCard(urlcard.cardId)
+    
+    #assert
+    assert result != None
+    assert urlcard.cardId == result["cardId"]
+    assert urlcard.Urls == result["Urls"]
